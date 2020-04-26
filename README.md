2495efc75dae528183b0328d3c1a3373aef610be
takaaa220 <taaaaaaaaaa220@gmail.com>
2020-04-26 15:08:44 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# parcel-react-ts
+React, TypeScriptのための環境を簡単に構築するためのリポジトリ
+バンドラーにParcelを用いている
---
+++ src/index.tsx
@@ -0,0 +1,5 @@
+import * as React from "react";
+import { render } from "react-dom";
+
+const App: React.SFC = () => <div>App</div>;
+render(<App />, document.getElementById("root"));
